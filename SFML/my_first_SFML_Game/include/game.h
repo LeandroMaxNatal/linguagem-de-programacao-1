61e2a48a68a95ec3e52a327c4eaf44da7129c983
LeandroMaxNatal <leandromaxnatal@gmail.com>
2015-07-13 16:04:09 -0300

    Player movendo-se pela tela

--- SFML/my_first_SFML_Game/include/game.h
+++ SFML/my_first_SFML_Game/include/game.h
@@ -13,9 +13,12 @@ private:
 	void processEvents();
 	void update();
 	void render();
+
+	void handlePlayerInput( sf::Keyboard::Key key, bool isPressed );
 private:
 	sf::RenderWindow mWindow;
 	sf::CircleShape mPlayer;
+	bool mIsMovingUp, mIsMovingDown, mIsMovingRight, mIsMovingLeft;
 };
 
 #endif
--- SFML/my_first_SFML_Game/sources/game.cpp
+++ SFML/my_first_SFML_Game/sources/game.cpp
@@ -2,7 +2,8 @@
 
 Game::Game()
 : mWindow(sf::VideoMode(640,480), "Meu primeiro Jogo")
-, mPlayer()
+, mPlayer(), mIsMovingUp(false), mIsMovingDown(false)
+, mIsMovingLeft(false), mIsMovingRight(false)
 {
 	mPlayer.setRadius(40.f);
 	mPlayer.setPosition(100.f, 100.f);
@@ -26,15 +27,36 @@ void Game::processEvents()
 	sf::Event event;
 	while(mWindow.pollEvent(event))
 	{
-		if( event.type == sf::Event::Closed )
-			mWindow.close();
+		switch( event.type )
+		{
+			case sf::Event::KeyPressed:
+				handlePlayerInput( event.key.code, true );
+				break;
+			case sf::Event::KeyReleased:
+				handlePlayerInput( event.key.code, false );
+				break;
+			case sf::Event::Closed:
+				mWindow.close();
+				break;
+		}
 	}	
 }
 
 
 void Game::update()
 {
-	/* Vazio por enquanto */
+	sf::Vector2f movement(0.f, 0.f);
+
+	if ( mIsMovingUp )
+		movement.y -= 1.f;
+	if ( mIsMovingDown )
+		movement.y += 1.f;
+	if ( mIsMovingLeft )
+		movement.x -= 1.f;
+	if ( mIsMovingRight )
+		movement.x += 1.f;
+
+	mPlayer.move( movement );
 }
 
 
@@ -44,3 +66,18 @@ void Game::render()
 	mWindow.draw(mPlayer);
 	mWindow.display();
 }
+
+
+void Game::handlePlayerInput( sf::Keyboard::Key key, bool isPressed )
+{
+	if ( key == sf::Keyboard::W )
+		mIsMovingUp = isPressed;
+	else if ( key == sf::Keyboard::S )
+		mIsMovingDown = isPressed;
+	else if ( key == sf::Keyboard::D )
+		mIsMovingRight = isPressed;
+	else if ( key == sf::Keyboard::A )
+		mIsMovingLeft = isPressed;
+}
+
+/**** ===================================================================[ END of game.cpp ]===================================================================****/
